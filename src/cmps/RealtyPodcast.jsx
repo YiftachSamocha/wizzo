a777b52c594164277b1840c356154f191ad10e26
Yiftach Samocha <yiftachsamo@gmail.com>
2025-08-09 00:59:19 +0300

    Design podcast cmp

--- src/cmps/RealtyPodcast.jsx
+++ src/cmps/RealtyPodcast.jsx
@@ -1,11 +1,35 @@
+import podcasts from '../data/podcasts.json'
+import headphonesImg from '../assets/img/headphones.png'
+import playerImg from '../assets/img/player.png'
+import { RealtyCategory } from './RealtyCategory'
 
-const podcasts = [1, 2, 3, 4]
 export function RealtyPodacst() {
     return <section className="podcast">
-     
+        <RealtyCategory category={{ title: 'פודקאסטים', color: '#d10e89' }} />
+
+
         <div className="podcast-list">
-            {podcasts.map(() => {
-                return <div className="podcast-item"></div>
+            {podcasts.map(podcast => {
+                return <div className="podcast-item">
+                    <div className='podcast-icons'>
+                        <img src={headphonesImg} />
+                        <img src={playerImg} />
+                    </div>
+
+                    <div className='podcast-info'>
+                        <div>
+                            <p className='host'>{podcast.host}</p>
+                            <p className='host-info'>{podcast.hostInfo}</p>
+                            <p className='quote'>"{podcast.quote}"</p>
+                        </div>
+                        <p className='details'>פודקאסטים | {podcast.date} | מערכת מרכז הנדל"ן</p>
+                    </div>
+                    <div className="podcast-cont">
+                        <img src={podcast.img} />
+                        <div>פודקאסטים</div>
+                    </div>
+
+                </div>
             })}
 
 
